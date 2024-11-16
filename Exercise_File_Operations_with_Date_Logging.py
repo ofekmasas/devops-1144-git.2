846c16e1201104b72dcdcd2a5c42fa38e11176d5
ofekmasas <ofekmasaspro@gmail.com>
2024-11-16 22:03:17 +0200

    commit

--- Exercise_File_Operations_with_Date_Logging.py
+++ Exercise_File_Operations_with_Date_Logging.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+############################task1
 if(not os.path.exists(r"C:\Users\ofekp\OneDrive\שולחן העבודה\journal.txt")):
     journal=open(r"C:\Users\ofekp\OneDrive\שולחן העבודה\journal.txt",'x')
 
@@ -12,10 +13,35 @@ while journal_entries !='stop':
     journal.write(journal_entries + '\n')
     journal_entries=input("Please enter a few lines (to stop type 'stop'):\n")
 journal.close()
-   
+
+############################task2
 print('The lines you wrote: \n')
 jornal = open(r"C:\Users\ofekp\OneDrive\שולחן העבודה\journal.txt",'r')
 print(jornal.read())
 
+############################task3
+journal=open(r"C:\Users\ofekp\OneDrive\שולחן העבודה\journal.txt",'a')
+journal_entries=input("Please enter a few more lines (to stop type 'stop'):\n")
 
+while journal_entries !='stop':
+    journal.write(str(datetime.datetime.now()) + ' ')
+    journal.write(journal_entries + '\n')
+    journal_entries=input("Please enter a few more lines (to stop type 'stop'):\n")
+
+############################task4
+jornal=open(r"C:\Users\ofekp\OneDrive\שולחן העבודה\journal.txt",'r')
+
+
+lines = len(jornal.readlines())
+print(f'The number of lines: {lines}')
+
+
+
+
+
+for line in lines:
+    len(line.split())
+sum = 
+words= sum()
+print('Number of words: ' + words)
 
--- date and time.py
+++ date and time.py
@@ -1,3 +1,1 @@
-import datetime
-current_datetime = datetime.datetime.now()
-print(datetime.datetime.now())
+lines = ['2024-11-16 21:12:37.261978 yonatan\n', '2024-11-16 21:12:42.626825 halach lagan\n', '2024-11-16 21:12:49.404463 rahamim\n', '2024-11-16 21:12:51.246973 lo\n', '2024-11-16 21:12:53.101098 yada\n']
