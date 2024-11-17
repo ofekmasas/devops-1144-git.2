867ec8f67af343eaa2b14798ee1b3d86786da426
ofekmasas <ofekmasaspro@gmail.com>
2024-11-17 22:33:00 +0200

    commit

--- Logging Internet Speed to a CSV File.py
+++ Logging Internet Speed to a CSV File.py
@@ -1,3 +1,26 @@
-import speedtest-cli
+import os
+import speedtest
+import datetime
+import time
+
+#ping=speedtest.Speedtest.ping    
+
+def Logging_Internet_Speed_to_a_CSV_File(filepath):
+    if not os.path.exists(filepath):
+        filepath= open(filepath,'x')
+    filepath=open(filepath,'a')
+    filepath.write("Timestamp,Download Speed (Mbps),Upload Speed (Mbps)\n")
+    counter=0
+    while True:
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        download=round(st.download() / 1000000, 2)
+        upload=round(st.upload() / 1000000 , 2)
+        filepath.write(f"{str(datetime.datetime.now())},{str(download)},{str(upload)}\n")
+        counter+=1
+        print(f"cycle: {counter}\ndownload: {download}\nupload: {upload}")
+        time.sleep(5)
+
+Logging_Internet_Speed_to_a_CSV_File('speed log.csv')
 
 
