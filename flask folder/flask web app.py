e911a786f78dd431de04655f25332a1e99c2d199
ofekmasas <ofekmasaspro@gmail.com>
2024-11-20 13:23:25 +0200

    .

--- flask folder/flask web app.py
+++ flask folder/flask web app.py
@@ -1,23 +1,18 @@
-# app.py
-
-from flask import Flask, render_template
-import random
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-def get_random_quote():
-    response = requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
-    if response.status_code == 200:
-        quote_data = response.json()
-        return f"{quote_data['quoteText']} - {quote_data['quoteAuthor']}"
-    return "Unable to fetch a quote at the moment."
+# Home Route
+@app.route("/")
+def home():
+    return "This is the homepage of ofek's website"
 
-@app.route('/')
-def index():
-    # random_quote = random.choice(quotes)
-    random_quote = get_random_quote()
-    return render_template('index.html', quote=random_quote)
+# Greeting Route
+@app.route("/greet/<name>")
+def greet(name):
+    if name!='about':
+        return f"Hello, {name}!"
+    return 'This is a sample website created by ofek'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
